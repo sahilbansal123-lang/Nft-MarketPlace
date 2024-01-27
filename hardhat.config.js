d6da59242fb60cad7c10e452c599c80da3b456cb
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-01-27 10:31:36 +0530

    deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-const fs = require('fs');
+const fs = require("fs");
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -15,20 +15,22 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     goerli: {
-      url: "<YOUR_ALCHEMY_URL>",
-      accounts: [ "<YOUR_PRIVATE_KEY>" ]
-    }
+      url: "https://eth-sepolia.g.alchemy.com/v2/ZH4-jaHunjk7sGoTnht3OTgYIBdit13h",
+      accounts: [
+        "b49de62fdd943b7120a8a335a0aa2f7cce34debd0b0a4b0412b5d800ab6c1774",
+      ],
+    },
   },
   solidity: {
     version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
+        runs: 200,
+      },
+    },
+  },
 };
